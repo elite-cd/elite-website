17407682dd97db940670c967b211d2fd08093f68
Joel Lokadi <nielsjoelok2@gmail.com>
2023-03-14 16:12:26 +0100

    add getImage() to gatsbyImage component

--- src/components/pages/CoursePage/index.js
+++ src/components/pages/CoursePage/index.js
@@ -11,39 +11,39 @@ import { ButtonRounded } from "../../Button";
 import Page from "../../PageTemplate";
 import * as style from "./CoursePage.module.scss";
 
-import img1_base_programmation from "../../../assets/images/pexels-katerina-holmes-5905700.jpg"
-import img2_base_programmation from "../../../assets/images/pexels-anna-tarazevich-14751274.jpg"
+import img1_base_programmation from "../../../assets/images/pexels-katerina-holmes-5905700.jpg";
+import img2_base_programmation from "../../../assets/images/pexels-anna-tarazevich-14751274.jpg";
 
-import img1_aplhabetisation_num from "../../../assets/images/img_2_b.jpg"
-import img2_aplhabetisation_num from "../../../assets/images/pexels-mart-production-7550542.jpg"
+import img1_aplhabetisation_num from "../../../assets/images/img_2_b.jpg";
+import img2_aplhabetisation_num from "../../../assets/images/pexels-mart-production-7550542.jpg";
 
-import img1_immersion from "../../../assets/images/pexels-mart-production-7550298.jpg"
-import img2_immersion from "../../../assets/images/pexels-cottonbro-studio-5076519.jpg"
+import img1_immersion from "../../../assets/images/pexels-mart-production-7550298.jpg";
+import img2_immersion from "../../../assets/images/pexels-cottonbro-studio-5076519.jpg";
 
 const CoursePage = ({ course, otherCourses }) => {
   const outcomesList = course.outcomes.split(",");
 
   const imgList = {
     img1: "",
-    img2: ""
-  }
+    img2: "",
+  };
 
   switch (course.slug) {
     case "immersion-en-entreprise":
-        imgList.img1 = img1_immersion
-        imgList.img2 = img2_immersion      
+      imgList.img1 = img1_immersion;
+      imgList.img2 = img2_immersion;
       break;
 
     case "base-programmation":
-        imgList.img1 = img1_base_programmation
-        imgList.img2 = img2_base_programmation
+      imgList.img1 = img1_base_programmation;
+      imgList.img2 = img2_base_programmation;
       break;
-    
+
     case "alphabetisation-numerique":
-        imgList.img1 = img2_aplhabetisation_num
-        imgList.img2 = img1_aplhabetisation_num
+      imgList.img1 = img2_aplhabetisation_num;
+      imgList.img2 = img1_aplhabetisation_num;
       break;
-  
+
     default:
       break;
   }
@@ -53,26 +53,28 @@ const CoursePage = ({ course, otherCourses }) => {
       <div className={style.container}>
         <div className={style.header}></div>
         <section className={style.hero}>
-          
-          <StaticImage
+          <GatsbyImage
+            objectFit={"cover"}
+            alt="Image 2 Loading failed"
             className={style.hero__image}
-            src={ imgList.img2 }
+            image={getImage(imgList.img2)}
           />
 
           <div className={style.hero__overlay}>
             <div className={style.overlay_left}>
+              <Link to={ROUTES.SIGNUP} className={style.customRoundedBtn}>
+                <div style={{ height: "100%", width: "100%" }}>
+                  <div style={{ height: "30%" }}></div>
 
-            <Link to={ROUTES.SIGNUP} className={ style.customRoundedBtn }>
-                    <div style={{ height: "100%", width: "100%" }}>
-                      <div style={{ height: "30%" }}></div>
-                        
-                      <div style={{ height: "40%", display: "inline-flex" }}>
-                          Démarez votre formation &nbsp;
-                        <span style={{ marginTop: "1%" }}><FontAwesomeIcon icon={  faCircleArrowRight  } /></span>
-                      </div>
+                  <div style={{ height: "40%", display: "inline-flex" }}>
+                    Démarez votre formation &nbsp;
+                    <span style={{ marginTop: "1%" }}>
+                      <FontAwesomeIcon icon={faCircleArrowRight} />
+                    </span>
+                  </div>
 
-                      <div style={{ height: "30%" }}></div>
-                    </div>
+                  <div style={{ height: "30%" }}></div>
+                </div>
               </Link>
             </div>
             <div className={style.overlay_right}>
@@ -101,15 +103,20 @@ const CoursePage = ({ course, otherCourses }) => {
               return (
                 <div className={style.other__item}>
                   <Link to={courseLink}>
-                   
-                      <div style={{ width: "50%", marginRight: "auto", marginLeft: "auto" }}>
-                        <GatsbyImage
-                          objectFit={"contain"}
-                          alt={`${item.title} image`}
-                          className={style.other__image}
-                          image={getImage(item.image)}
-                        />
-                      </div>
+                    <div
+                      style={{
+                        width: "50%",
+                        marginRight: "auto",
+                        marginLeft: "auto",
+                      }}
+                    >
+                      <GatsbyImage
+                        objectFit={"contain"}
+                        alt={`${item.title} image`}
+                        className={style.other__image}
+                        image={getImage(item.image)}
+                      />
+                    </div>
 
                     <p className={style.other__title}> {item.title} </p>
                     <p className={style.other__timeline}>{item.timeline}</p>
@@ -123,11 +130,11 @@ const CoursePage = ({ course, otherCourses }) => {
             })}
           </div>
           <div className={style.course__detail}>
-            <StaticImage
-              className={style.course__preview}
-              src={ imgList.img1 }
-
+            <GatsbyImage
               objectFit={"cover"}
+              alt="Image 1 Loading failed"
+              className={style.course__preview}
+              image={getImage(imgList.img1)}
             />
             <p className={style.preview__title}>Overview</p>
             <p className={style.preview__description}>{course.description}</p>
