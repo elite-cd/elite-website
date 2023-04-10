1a4275adeb7d285b0cb6bc6edab163ddc2a2334d
philippembambi <philippembambi413@gmail.com>
2023-04-10 12:12:19 +0100

    add notification

--- src/components/pages/Home/index.js
+++ src/components/pages/Home/index.js
@@ -1113,6 +1113,81 @@ const Home = ({ carouselItems, map }) => {
             </p>
           </p>
 
+          <div
+            data-te-modal-init
+            class="fixed left-0 top-0 z-[1055] hidden h-full w-full overflow-y-auto overflow-x-hidden outline-none"
+            id="rightBottomModal"
+            tabindex="-1"
+            aria-labelledby="rightBottomModalLabel"
+            aria-hidden="true"
+          >
+            <div
+              data-te-modal-dialog-ref
+              class="pointer-events-none absolute bottom-7 right-7 h-auto w-full translate-x-[100%] opacity-0 transition-all duration-300 ease-in-out min-[576px]:mx-auto min-[576px]:mt-7 min-[576px]:max-w-[500px]"
+            >
+              <div class="min-[576px]:shadow-[0_0.5rem_1rem_rgba(#000, 0.15)] pointer-events-auto relative flex w-full flex-col rounded-md border-none bg-white bg-clip-padding text-current shadow-lg outline-none">
+                <div class="flex flex-shrink-0 items-center justify-between rounded-t-md border-b-2 border-neutral-100 border-opacity-100 p-4">
+                  <h5
+                    class="text-xl font-medium leading-normal text-neutral-800"
+                    id="exampleModalLabel"
+                  >
+                    Notification
+                  </h5>
+                  <button
+                    type="button"
+                    class="box-content rounded-none border-none hover:no-underline hover:opacity-75 focus:opacity-100 focus:shadow-none focus:outline-none"
+                    data-te-modal-dismiss
+                    aria-label="Close"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke-width="1.5"
+                      stroke="currentColor"
+                      class="h-6 w-6"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M6 18L18 6M6 6l12 12"
+                      />
+                    </svg>
+                  </button>
+                </div>
+
+                <div class="relative flex-auto p-4" data-te-modal-body-ref>
+                  Votre message a été envoyé avec succès ! Nous vous répondrons
+                  dans le bref délai.
+                </div>
+
+                <div class="flex flex-shrink-0 flex-wrap items-center justify-end rounded-b-md border-t-2 border-neutral-100 border-opacity-100 p-4">
+                  <button
+                    type="button"
+                    class="inline-block rounded bg-teal-800 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white transition duration-150 ease-in-out active:bg-primary-accent-200"
+                    data-te-modal-dismiss
+                    data-te-ripple-init
+                    data-te-ripple-color="light"
+                  >
+                    Ok
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <button
+            type="button"
+            class="hidden inline-block rounded bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)]"
+            data-te-toggle="modal"
+            data-te-target="#rightBottomModal"
+            data-te-ripple-init
+            data-te-ripple-color="light"
+            id="popupBtn"
+          >
+            Bottom right
+          </button>
+
           <form ref={form}>
             <div class="relative mb-4 flex flex-wrap items-center justify-center">
               <input
