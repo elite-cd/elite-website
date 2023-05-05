7af29ee2ab4a709d6e362598e5de274cea892315
philippembambi <philippembambi413@gmail.com>
2023-05-05 17:24:35 +0100

    fix main image

--- src/components/pages/About/index.js
+++ src/components/pages/About/index.js
@@ -848,7 +848,6 @@ const About = () => {
                     src={team4}
                     class="mx-auto mb-4 w-40 h-40 rounded-full bg-gray-200"
                     alt="Avatar"
-                    style={{ objectFit: "fill" }}
                   />
                   <h5 class="mb-2 text-xl font-medium leading-tight text-teal-800">
                     Christian Kayembe
